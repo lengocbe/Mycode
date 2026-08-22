9e4321d7dab50bca85e3c753148673abda583a00
lengocbe <107679785+lengocbe@users.noreply.github.com>
2026-08-22 13:23:54 +0700

    Add SoThuChi Android: app/src/main/java/vn/sothuchi/app/TransactionItem.java

---
+++ SoThuChiAndroid/app/src/main/java/vn/sothuchi/app/TransactionItem.java
@@ -0,0 +1,20 @@
+package vn.sothuchi.app;
+
+public final class TransactionItem {
+    public final long id;
+    public final String type;
+    public final long amount;
+    public final String category;
+    public final String note;
+    public final String date;
+
+    public TransactionItem(long id, String type, long amount, String category, String note, String date) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.category = category;
+        this.note = note;
+        this.date = date;
+    }
+}
+
